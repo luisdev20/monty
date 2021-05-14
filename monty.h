d3da510ebb0f4d6fdea78771b1da9fa5efdbc9b4
luisdev20 <lescobedo.im@gmail.com>
2021-05-14 03:43:28 +0000

    Adding stack operation prototypes

--- monty.h
+++ monty.h
@@ -51,4 +51,18 @@ int len_chars(FILE *);
 int interpret_line(char *, int, int);
 void find_func(char *, char *, int, int);
 
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+
+void call_fun(op_func, char *, char *, int, int);
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+
+
 #endif /*__MONTY_H__*/
