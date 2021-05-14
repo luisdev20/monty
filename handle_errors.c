e33225aa5ed49854f7b542b266d332ee2d179afe
luisdev20 <lescobedo.im@gmail.com>
2021-05-14 04:40:28 +0000

    handle tabs

--- handle_errors.c
+++ handle_errors.c
@@ -18,26 +18,26 @@ void err(int error_code, ...)
 	va_start(ag, error_code);
 	switch (error_code)
 	{
-	case 1:
-		printf("USAGE: monty file\n");
-		break;
-	case 2:
-		printf("Error: Can't open file %s\n",
-		       va_arg(ag, char *));
-		break;
-	case 3:
-		l_num = va_arg(ag, int);
-		op = va_arg(ag, char *);
-		printf("L%d: unknown instruction %s\n", l_num, op);
-		break;
-	case 4:
-		printf("Error: malloc failed\n");
-		break;
-	case 5:
-		printf("L%d: usage: push integer\n", va_arg(ag, int));
-		break;
-	default:
-		break;
+		case 1:
+			printf("USAGE: monty file\n");
+			break;
+		case 2:
+			printf("Error: Can't open file %s\n",
+			       va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			printf("L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			printf("Error: malloc failed\n");
+			break;
+		case 5:
+			printf("L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
 	}
 	free_nodes();
 	exit(EXIT_FAILURE);
@@ -60,25 +60,25 @@ void more_err(int error_code, ...)
 	va_start(ag, error_code);
 	switch (error_code)
 	{
-	case 6:
-		printf("L%d: can't pint, stack empty\n",
-		       va_arg(ag, int));
-		break;
-	case 7:
-		printf("L%d: can't pop an empty stack\n",
-		       va_arg(ag, int));
-		break;
-	case 8:
-		l_num = va_arg(ag, unsigned int);
-		op = va_arg(ag, char *);
-		printf("L%d: can't %s, stack too short\n", l_num, op);
-		break;
-	case 9:
-		printf("L%d: division by zero\n",
-		       va_arg(ag, unsigned int));
-		break;
-	default:
-		break;
+		case 6:
+			printf("L%d: can't pint, stack empty\n",
+			       va_arg(ag, int));
+			break;
+		case 7:
+			printf("L%d: can't pop an empty stack\n",
+			       va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			op = va_arg(ag, char *);
+			printf("L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		case 9:
+			printf("L%d: division by zero\n",
+				va_arg(ag, unsigned int));
+			break;
+		default:
+			break;
 	}
 	free_nodes();
 	exit(EXIT_FAILURE);
@@ -99,14 +99,14 @@ void string_err(int error_code, ...)
 	l_num = va_arg(ag, int);
 	switch (error_code)
 	{
-	case 10:
-		printf("L%d: can't pchar, value out of range\n", l_num);
-		break;
-	case 11:
-		printf("L%d: can't pchar, stack empty\n", l_num);
-		break;
-	default:
-		break;
+		case 10:
+			printf("L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			printf("L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
 	}
 	free_nodes();
 	exit(EXIT_FAILURE);
